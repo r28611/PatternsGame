06ab77914765a7113b24589cfb8acca3e88213e4
Margarita Novokh <r28611@gmail.com>
2021-05-11 23:35:06 +0200

    Initial Commit

---
+++ PatternsGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatternsGame
+//
+//  Created by Margarita Novokhatskaia on 11.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
