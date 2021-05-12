436ad8431b835bfb76fc9cfe1aaf3c2ad2187b69
Margarita Novokh <r28611@gmail.com>
2021-05-12 10:51:08 +0200

    made screens

---
+++ PatternsGame/ButtonsDesign.swift
@@ -0,0 +1,37 @@
+//
+//  ButtonsDesign.swift
+//  PatternsGame
+//
+//  Created by Margarita Novokhatskaia on 11.05.2021.
+//
+
+import UIKit
+
+class AnswerButton: UIButton {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        self.layer.cornerRadius = self.layer.frame.height / 4
+        self.backgroundColor = #colorLiteral(red: 0.5796732306, green: 0.5393779278, blue: 0.9125451446, alpha: 1)
+        self.setTitleColor(UIColor.white, for: .normal)
+        self.titleLabel?.font = UIFont(name: "Chalkduster", size: 24)
+        
+    }
+
+}
+
+class AdditionalButton: UIButton {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        self.layer.cornerRadius = self.layer.frame.height / 4
+        self.backgroundColor = UIColor.systemGray5
+        self.setTitleColor(#colorLiteral(red: 0.5796732306, green: 0.5393779278, blue: 0.9125451446, alpha: 1), for: .normal)
+        self.titleLabel?.font = UIFont(name: "Chalkduster", size: 20)
+        self.tintColor = #colorLiteral(red: 0.5796732306, green: 0.5393779278, blue: 0.9125451446, alpha: 1)
+        
+    }
+
+}
