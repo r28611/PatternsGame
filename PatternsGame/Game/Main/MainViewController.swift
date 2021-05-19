1beb79aeac5446aa6948a862ac47884a8e29d649
Margarita Novokh <r28611@gmail.com>
2021-05-19 17:23:16 +0200

    delete RusultsVC from storyboard

--- PatternsGame/Game/Main/MainViewController.swift
+++ PatternsGame/Game/Main/MainViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapResults(_ sender: UIButton) {
+        let viewController = ResultsViewController()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
 }
 
